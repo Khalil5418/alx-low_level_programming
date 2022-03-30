23ad294c7f8cba085fff328e9fd812baeac57dce
Khalil5418 <khalilsoumahoro@gmail.com>
2022-03-30 15:02:47 -0700

    Add C programs from 0 to 101

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+int check_path(char *S1, char *s2);
+char *move_char(char *s2);
+
+/**
+ * wildcmp - compares 2 strings
+ * @s1: string to be compared
+ * @s2: string to be compared
+ *
+ * Return: returns numeral value
+ */
+int wildcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	if (*s1 == '\0' && *s2 == '*' && !*move_char(s2))
+		return (1);
+	if (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+			return (1);
+	return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if (*s1 == '\0' || *s2 == '\0')
+		return (0);
+	if (*s2 == '*')
+	{
+		s2 = move_char(s2);
+		if (*s2 == '\0')
+			return (1);
+		if (*s1 == *s2)
+			i += wildcmp(s1 + 1, s2 + 1);
+		i += check_path(s1 + 1, s2);
+		return (!!i);
+	}
+	return (0);
+}
